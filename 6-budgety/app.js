1d2a25e8c330e6f3b70bd7a4f823b9e51daf9666
Yaroslav Mishchenko <yaroslavzn@gmail.com>
2019-02-22 23:37:04 +0200

    add expense and income constructors, add data structure

--- 6-budgety/app.js
+++ 6-budgety/app.js
@@ -1,19 +1,57 @@
 var budgetModel = (function() {
 
     // Some code
+    var Expense = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var Income = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var data = {
+        allItems: {
+            exp: [],
+            inc: [],
+        },
+        total: {
+            exp: 0,
+            inc: 0,
+        }
+    };
 
 })();
+
+function Income(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+}
 
 var budgetView = (function () {
 
     // Some code
     var DOMstrings = {
-
+        itemType: '.add__type',
+        itemDescription: '.add__description',
+        itemValue: '.add__value',
+        itemAdd: '.add__btn',
     };
 
     return {
         getInputData: function() {
-
+            return {
+                type: document.querySelector(DOMstrings.itemType).value,
+                description: document.querySelector(DOMstrings.itemDescription).value,
+                value: document.querySelector(DOMstrings.itemValue).value,
+            }
+        },
+        getDOMstrings: function() {
+            return DOMstrings;
         },
     };
 
@@ -21,9 +59,25 @@ var budgetView = (function () {
 
 var budgetController = (function (model, view) {
 
+
+
+    var setupEventListeners = function() {
+        var DOM = view.getDOMstrings();
+
+        document.querySelector(DOM.itemAdd).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function(e) {
+
+            if (e.keycode === 13 || e.which === 13) {
+                ctrlAddItem();
+            }
+        });
+    };
+
     var ctrlAddItem = function() {
 
         // 1. Get the field input data
+        var inputData = view.getInputData();
 
         // 2. Add the item to the budget model.
 
@@ -34,13 +88,14 @@ var budgetController = (function (model, view) {
         // 5. Display the budget on the UI
     };
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    return {
+        init: function() {
+            setupEventListeners();
 
-    document.addEventListener('keypress', function(e) {
-
-        if (e.keycode === 13 || e.which === 13) {
-            ctrlAddItem();
+            console.log('Application has started.');
         }
-    });
+    };
 
 })(budgetModel, budgetView);
+
+budgetController.init();
